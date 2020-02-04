12587aa4a72fee3a7d8e52f419f22cefdd768b74
Titus Weikama R <titus_weikama_r@the-Quiet-Place.24>
2020-02-04 15:15:31 +0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,31 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+import { NavigationComponent } from './navigation/navigation.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { AboutComponent } from './about/about.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { QuestionsComponent } from './questions/questions.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
+import { HeaderComponent } from './header/header.component';
+import { SpeakersComponent } from './speakers/speakers.component';
+
+
+const routes: Routes = [
+  { path: '',   redirectTo: '/Home', pathMatch: 'full' },
+  { path: 'Home', component: HeaderComponent},
+  { path: 'About', component: AboutComponent },
+  { path: 'Schedules', component: ScheduleComponent},
+  { path: 'Speakers', component: SpeakersComponent},
+  { path: 'Gallery', component: GalleryComponent },
+  { path: 'Faq', component: QuestionsComponent },
+  { path: 'Blog', component: BlogComponent  },
+  { path: 'Pricing', component: PricingComponent  },
+  { path: 'Contact', component: ContactComponent },
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { AboutComponent } from './about/about.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { QuestionsComponent } from './questions/questions.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { SpeakersComponent } from './speakers/speakers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    GalleryComponent,
+    PricingComponent,
+    AboutComponent,
+    ScheduleComponent,
+    QuestionsComponent,
+    BlogComponent,
+    ContactComponent,
+    FooterComponent,
+    HeaderComponent,
+    SpeakersComponent
   ],
   imports: [
     BrowserModule,
